887da15d2061f61b04e5cbacdde0f59763a87c4a
Francisco J. Moreno <franciscoj.sparcoltary@gmail.com>
2020-12-18 00:18:01 -0600

    [FIX] cannot find FiltersComponent

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -1,11 +1,16 @@
+import { FiltersComponent } from './filters/filters.component';
 import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
 import { ChipComponent } from './chip/chip.component';
 
-const Components = [HeaderComponent, CardComponent, ChipComponent, FiltersComponent];
-
+const Components = [
+  HeaderComponent,
+  CardComponent,
+  ChipComponent,
+  FiltersComponent,
+];
 
 @NgModule({
   declarations: [Components],
