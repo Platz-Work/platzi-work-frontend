cc5a2f2683c028fa251df2e8951633e6b126074f
WalterMXZ <waltz.mx@gmail.com>
2020-12-17 23:46:13 -0600

    [ADD] filters

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -2,8 +2,9 @@ import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
+import { FiltersComponent } from './filters/filters.component';
 
-const Components = [HeaderComponent, CardComponent];
+const Components = [HeaderComponent, CardComponent,FiltersComponent];
 
 @NgModule({
   declarations: [Components],
